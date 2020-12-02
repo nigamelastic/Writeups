a9d09a87e3b6bbfc7bee9303aad9305841220c2b
nigamelastic <nigamelastic@gmail.com>
2020-12-03 00:32:48 +0100

    fixed my stupidity

--- adventofCode/adventofcode2020/Day2/day2Second.py
+++ adventofCode/adventofcode2020/Day2/day2Second.py
@@ -15,11 +15,14 @@ def getCorrectPassword(stringInput):
         letter=stringInputLine[0].split()[1]
         rangeValue=stringInputLine[0].split()[0].split("-")
         password=stringInputLine[1]
-        firstIndex = int(rangeValue[0])-1
-        secondIndex = int(rangeValue[1])-1
-        
+        firstIndex = int(rangeValue[0])
+        secondIndex = int(rangeValue[1])
+        print(password[firstIndex])
+        print(password[secondIndex])
+        print(password)
         if( (letter==password[firstIndex]) ^ (letter==password[secondIndex]) ):
             count+=1
+            print("above is correct")
         
       
     return count
