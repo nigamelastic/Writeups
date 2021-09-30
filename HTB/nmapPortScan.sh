4ad65173188c8a23973124bef9a5587d3adb2a37
nigamelastic <nigamelastic@gmail.com>
2021-09-30 16:10:25 -0400

    added stuff

--- HTB/nmapPortScan.sh
+++ HTB/nmapPortScan.sh
@@ -20,9 +20,15 @@ fi
 
 ports=$(nmap -sU -sT -p0-65535 --min-rate=1000 -T4 $1 | grep ^[0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//)
 
+#ports=$(nmap -sU -sT -p0-65535 --min-rate=1000 -T4 -iL $1 | grep ^[0-9] | cut -d '/' -f1 | tr '\n' ',' | sed s/,$//)
+echo "the ports found are: $ports"
+
+
 # use -PN if nmap probes do not detect a host
 #  nmap -PN -sC -sV -p$ports $1 -oN $2  
-#nmap -A -sS -sC -sV -p$ports $1 -oN $2 
 
 nmap -A -v -p$ports $1 -oN $2 
+#_____________________ to run from a file_____________________________
+#nmap -A -v -p$ports -iL $1 -oN $2
+
 #nmap -A -sS -v -p$ports $1 -oN $2 
