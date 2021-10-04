9761459a93cd36dd0d766654ffd33641259b8bb4
nigamelastic <nigamelastic@gmail.com>
2021-10-03 21:35:11 -0400

    added writeup for Monitors

---
+++ HTB/Monitors/backup.sh
@@ -0,0 +1,11 @@
+cat /home/marcus/.backup/backup.sh
+#!/bin/bash
+
+backup_name="cacti_backup"
+config_pass="VerticalEdge2020"
+
+zip /tmp/${backup_name}.zip /usr/share/cacti/cacti/*
+sshpass -p "${config_pass}" scp /tmp/${backup_name} 192.168.1.14:/opt/backup_collection/${backup_name}.zip
+rm /tmp/${backup_name}.zip
+$ 
+
