b12451a00f37b53c5bea346abc5090854b1e0755
nigamelastic <nigamelastic@gmail.com>
2023-11-10 17:31:51 -0500

    added Nibbles

---
+++ HTB/Nibbles/DirectoryBF/10.10.10.75/nibbleblog/feed.php
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+<title>Nibbles</title>
+<subtitle>Yum yum</subtitle>
+<link href="http://10.10.10.134/nibbleblog/feed.php" rel="self" />
+<id>http://10.10.10.134/nibbleblog/feed.php</id>
+<updated>2023-11-10T21:24:38+00:00</updated>
+</feed>
